04fc6f45bb6fb9ec317a825fda3327d73ed36b72
Capriatto <michu-1@hotmail.com>
2019-08-29 03:08:58 -0500

    cambios para la reunión del 29-08-19 -c

--- models/plastic_surgery_sheet.py
+++ models/plastic_surgery_sheet.py
@@ -45,7 +45,7 @@ class PlasticSurgerySheet(models.Model):
     lastname = fields.Char(string='First Last Name')
     middlename = fields.Char(string='Second Name')
     surname = fields.Char(string='Second Last Name')
-    gender = fields.Selection([('male','Male'), ('female','Female')], string='Gender')
+    gender = fields.Selection([('male','Male'), ('female','Female')], string='Gender', related="patient_id.sex")
     birth_date = fields.Date(string='Birth Date')
     age = fields.Integer(string='Age', compute='_compute_age_meassure_unit')
     age_meassure_unit = fields.Selection([('1','Years'),('2','Months'),('3','Days')], string='Unit of Measure of Age',
