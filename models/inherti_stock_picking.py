342e9dd88d7d2af2da5364d82987f6be35f98175
rferrer <rferrer@isc-bunkerramo.com>
2020-05-25 19:56:55 -0400

    [MOD] Nuevo campo para totalizar costos

--- models/inherti_stock_picking.py
+++ models/inherti_stock_picking.py
@@ -9,13 +9,18 @@ from odoo import models, fields, api, _
 class InheritStockPicking(models.Model):
     _inherit = "stock.picking"
 
+    total_cost = fields.Float(string="Costo Total")
+
     
     @api.multi
     def cost_calculation(self):
         for moves in self.move_lines:
         	qty = 1
+        	cost = 0
         	if moves.quantity_done > 0:
         		moves.product_cost = moves.product_tmpl_id.standard_price * moves.quantity_done
+        		self.total_cost += moves.product_cost
+
 
 
 class InheritStockMove(models.Model):
